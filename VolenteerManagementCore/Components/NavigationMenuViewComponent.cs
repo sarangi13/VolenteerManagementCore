8aba1ef78892ca1504d8b3629a7935908908eead
Stephen Waldron <30762163+s-waldron@users.noreply.github.com>
2017-08-20 20:46:00 -0400

    Add files via upload

---
+++ VolenteerManagementCore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using VolenteerManagementCore.Models;
+
+namespace VolenteerManagementCore.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private IVolenteerRepository repository;
+
+        public NavigationMenuViewComponent(IVolenteerRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(repository.Volenteers
+                    .Select(v => v.ApprovalStatus)
+                    .Distinct()
+                    .OrderBy(v => v)
+                );
+        }
+    }
+}
